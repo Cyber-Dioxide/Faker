5d7be7feed4b7eb2a78f57c56483c47d5535a947
Saad <93708296+Cyber-Dioxide@users.noreply.github.com>
2022-03-29 23:08:27 +0500

    Add files via upload

--- files/banner.py
+++ files/banner.py
@@ -1,4 +1,6 @@
 import os
+from files.colors import *
+
 
 logo = f"""
    ▄████████    ▄████████    ▄█   ▄█▄    ▄████████    ▄████████ 
@@ -17,25 +19,27 @@ logo = f"""
 ▀▀███ ████▄  ▀▀███▀▀▀     ███   ███                             
   ███    ███   ███    █▄  ███   ███                             
   ███    ███   ███    ███ ███   ███                             
-  ████████▀    ██████████  ▀█   █▀                              
-                                                                                                             
+  ████████▀    ██████████  ▀█   █▀   
+
+            {c}<{w}/{c}> {y}Author: {g}Saad Khan {r}| {g}Cyber-Dioxide
+
+
+{ran}[ + ] Instagram @cyber_dioxide
+{c}[ + ] Coding Instagram @cyber_dioxide_
+{y}[ + ] Github: Cyber-Dioxide                                                                                                          
 """
 
-from files.colors import *
+
 
 
 def banner():
     print(ran + logo)
-    print(ran + "\n","|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX,  "- " * 4, " [+] Follow me on Instagram @saadkhan041 ", "- " * 4 + ran + "|")
-    print(ran +"\n", "|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX,  "- " * 4, " [+] Follow me on Instagram @coding_memz ", "- " * 4+ran + "|")
-    print(ran +"\n", "|"+ Style.BRIGHT + Fore.LIGHTRED_EX,  "- " * 4, "[+] Github: https://github.com/Saadkhan041/ ", "- " * 3+ran + "|")
-
 
 def banner2():
 
-    print(ran + "\n","|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX,  "- " * 4, " [+] Follow me on Instagram @saadkhan041 ", "- " * 4 + ran + "|")
-    print(ran + "\n","|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX, "- " * 4, " [+] Follow me on Instagram @coding_memz ", "- " * 4+ran + "|")
-    print(ran + "\n","|"+ Style.BRIGHT + Fore.LIGHTRED_EX,  "- " * 4, "[+] Github: https://github.com/Saadkhan041/ ", "- " * 3+ran + "|")
+    print(ran + "\n","|"+ Style.BRIGHT + Fore.LIGHTCYAN_EX,  "- " * 4, " [+] Follow me on Instagram @cyber_dioxide ", "- " * 4 + ran + "|")
+    print(ran + "\n","|"+ Style.BRIGHT + Fore.LIGHTYELLOW_EX, "- " * 4, " [+] Follow me on Instagram @cyber_dioxide_ ", "- " * 4+ran + "|")
+    print(ran + "\n","|"+ Style.BRIGHT + Fore.LIGHTRED_EX,  "- " * 4, "[+] Github: https://github.com/Cyber-Dioxide/ ", "- " * 3+ran + "|")
 
 def clear():
     os.system("clear")
